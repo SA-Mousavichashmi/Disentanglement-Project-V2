0403ce44ff5a631fc06cc4ba60ba52f816994984
Seyedamin Mousavichashmi <76259887+SA-Mousavichashmi@users.noreply.github.com>
2025-04-18 18:59:19 +0330

    update progress bar logging in basetrainer.py

--- trainers/basetrainer.py
+++ trainers/basetrainer.py
@@ -31,6 +31,9 @@ class BaseTrainer():
 
     is_progress_bar: bool, optional
         Whether to use a progress bar for training
+
+    progress_bar_log_interval: int, optional
+        Update the progress bar with losses every `progress_bar_log_interval` iterations.
     """
 
     def __init__(self,
@@ -39,13 +42,16 @@ class BaseTrainer():
                  optimizer,
                  scheduler,
                  device,
-                 is_progress_bar=True): 
+                 is_progress_bar=True,
+                 progress_bar_log_interval=10  # Add log_interval parameter for updating progress bar based iterations
+                 ): 
 
         self.device = device
         self.model = model.to(self.device)
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.is_progress_bar = is_progress_bar  # Store is_progress_bar
+        self.progress_bar_log_interval = progress_bar_log_interval # Store log_interval
 
         if scheduler is None:
             ### Using constant scheduler with no warmup
@@ -64,7 +70,7 @@ class BaseTrainer():
         ----------
         data_loader: torch.utils.data.DataLoader
 
-        epochs: int
+        num_epochs: int
             Number of epochs to train the model for.
         """
         self.model.train()
@@ -89,25 +95,28 @@ class BaseTrainer():
 
         Return
         ------
-        mean_epoch_loss: float
-            Mean loss per image
+        epoch_to_log: dict
+            Dictionary containing the mean of logged values for the epoch.
         """
         epoch_to_log = collections.defaultdict(list)
+        num_batches = len(data_loader) # Get total number of batches
 
         # Added kwargs for trange
         kwargs = dict(desc="Epoch {}".format(epoch + 1),
                       leave=False,
                       disable=not self.is_progress_bar)
         # Wrap loop with trange
-        with trange(len(data_loader), **kwargs) as t:
-            for _, data_out in enumerate(data_loader):
+        with trange(num_batches, **kwargs) as t:
+            for i, data_out in enumerate(data_loader): # Use enumerate to get iteration index 'i'
                 data = data_out[0]
                 iter_out = self._train_iteration(data)
                 
                 for key, item in iter_out['to_log'].items():
                     epoch_to_log[key].append(item)
 
-                t.set_postfix(**iter_out['to_log'])
+                # Update progress bar postfix only at progress_bar_log_interval or last iteration
+                if (i + 1) % self.progress_bar_log_interval == 0 or (i + 1) == num_batches:
+                    t.set_postfix(**iter_out['to_log'])
                 t.update()
 
         return {key: np.mean(item) for key, item in epoch_to_log.items()} # take the mean of the logged values for each epoch
@@ -158,6 +167,8 @@ class BaseTrainer():
         elif self.loss_fn.mode == 'optimizes_internally':
             loss_out = self.loss_fn(samples, self.model, self.optimizer)
             loss = loss_out['loss']
+        else:
+            raise ValueError(f"Unknown loss function mode: {self.loss_fn.mode}")
 
         # Extract any logged metrics and return both loss and logs
         to_log = loss_out.get('to_log', {})
