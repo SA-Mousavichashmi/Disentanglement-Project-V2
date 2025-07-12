94ef4bcecca57d1958989497c6e5c5b606ead3b6
Seyedamin Mousavichashmi <76259887+SA-Mousavichashmi@users.noreply.github.com>
2025-07-12 11:44:53 +0330

    Add DIP-VAE loss implementation and update loss selection

--- losses/__init__.py
+++ losses/__init__.py
@@ -7,7 +7,7 @@ LICENSE file in the root directory of this source tree.
 """
 
 # Only include implemented and valid loss names
-LOSS_LIST = ['betavae', 'annealedvae', 'factorvae', 'betatcvae', 'beta_toroidal_vae', 'group_theory', 'beta_s_n_vae']
+LOSS_LIST = ['betavae', 'annealedvae', 'factorvae', 'betatcvae', 'beta_toroidal_vae', 'group_theory', 'beta_s_n_vae', 'dipvae']
 RECON_DISTS = ["bernoulli", "laplace", "gaussian"]
 
 def select(name, **kwargs):
@@ -33,6 +33,9 @@ def select(name, **kwargs):
     if name == 'beta_s_n_vae':
         from losses.s_n_vae.beta_s_n_vae import BetaSNVAELoss
         return BetaSNVAELoss(**kwargs)
+    if name == 'dipvae':
+        from losses.n_vae.dipvae import Loss
+        return Loss(**kwargs)
     
     err = "Unknown loss.name = {}. Possible values: {}"
     raise ValueError(err.format(name, LOSS_LIST))
